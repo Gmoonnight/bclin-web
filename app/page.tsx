730214783458a50ed28fb4167490c8ed56175aa6
Delin Zhao <605769581@qq.com>
2024-02-08 19:26:40 +0800

    add base login page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,27 @@ export default function Home() {
 			style = {{
 				backgroundImage: `url('/home_bg.jpg')`
 			}}
-			className = 'w-screen h-screen bg-cover bg-center'
+			className = 'w-screen h-screen bg-cover bg-center relative'
 		>
-    	</main>
+			<div className = 'absolute left-0 right-0 top-0 bottom-0 m-auto border w-96 h-96 rounded-md'>
+				<form>
+					<input type = "text" placeholder = "请输入用户名" 
+						className = 'absolute w-80 h-11 left-0 right-0 mx-auto rounded-md outline-0 opacity-85 mt-20 pl-3 
+									hover:border-white-300 hover:border'
+					/>
+					<input type = "password" placeholder = "请输入密码"
+						className = 'absolute w-80 h-11 left-0 right-0 mx-auto rounded-md outline-0 opacity-85 mt-40 pl-3
+									hover:border-white-300 hover:border'
+					/>
+					<button 
+						className = 'absolute mt-60 left-0 right-0 mx-auto  w-20 h-8
+									bg-pink-400 hover:bg-pink-500 rounded-md'
+					>
+						登 录
+					</button>
+				</form>
+			</div>
+
+		</main>
   	)
 }
